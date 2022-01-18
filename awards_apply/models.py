b68ec1d4d3d3e137e6ad020e8670761b9c2ba403
yml <1138194104@qq.com>
2022-01-18 20:31:31 +0800

    feature: 新建管理员表,秘书表修改 etc. (#7)
    feature: 新建管理员表,秘书表修改 etc. (#7)
    
    feature: 新建管理员表,秘书表修改 etc. (#7)
    
    feature: 新建管理员表,秘书表修改 etc. (#7)

--- awards_apply/models.py
+++ awards_apply/models.py
@@ -20,10 +20,30 @@ class Awards(models.Model):
     ]
     approval_state = models.IntegerField(choices=APPROVAL_STATE, default=3, verbose_name="评审状态")
 
+    def to_json(self):
+        return {
+            "id": self.id,
+            "award_name": self.award_name,
+            "award_level": self.award_level,
+            "award_description": self.award_description,
+            "award_consultant": self.award_consultant,
+            "award_image": self.award_image.name,
+            "create_time": str(self.create_time).split('+')[0],
+            "start_time": str(self.start_time).split('+')[0],
+            "end_time": str(self.end_time).split('+')[0],
+            "approval_state": self.approval_state,
+        }
+
 
 class Secretary(models.Model):
-    user_id = models.CharField(max_length=128, verbose_name="用户id")
-    group_id = models.CharField(max_length=128, verbose_name="组id")
+    group_id = models.IntegerField(verbose_name="组id", unique=True)
+    secretaries = json_models.JSONField(verbose_name="组内秘书", default=list)
+
+
+# 管理员表
+class Manager(models.Model):
+    group_id = models.IntegerField(verbose_name="组id", unique=True)
+    managers = json_models.JSONField(verbose_name="管理员列表", default=list)
 
 
 # 申请表
@@ -31,7 +51,7 @@ class AwardApplicationRecord(models.Model):
     award_id = models.IntegerField(verbose_name="奖项id")
     application_time = models.DateTimeField(verbose_name="申请时间")
     application_reason = models.TextField(verbose_name="申请理由")
-    application_id = json_models.JSONField(verbose_name="申请人id列表")
+    application_users = json_models.JSONField(verbose_name="申请人username列表", default=list)
     application_attachments = json_models.JSONField(verbose_name="申请附件地址列表")
     APPROVAL_STATE = [
         (0, "待评审"),
@@ -42,6 +62,6 @@ class AwardApplicationRecord(models.Model):
     approval_state = models.IntegerField(
         choices=APPROVAL_STATE, default=3, verbose_name="评审状态"
     )
-    approval_id = models.IntegerField(verbose_name="评审人id", null=True)
+    approval_user = models.CharField(max_length=50, verbose_name="评审人username", null=True)
     approval_time = models.DateTimeField(verbose_name="评审时间", null=True)
     approval_text = models.TextField(verbose_name="评审评语", null=True)
