f1f80fe7688c1fbf95ac1d3be1d7c7674d90d8a4
yml <1138194104@qq.com>
2022-02-17 15:14:00 +0800

    feature: 新增我的审批, 审批申请接口

--- awards_apply/utils/exception_handler.py
+++ awards_apply/utils/exception_handler.py
@@ -1,16 +1,39 @@
+from django.core.exceptions import PermissionDenied
+from django.http import Http404
+from rest_framework import exceptions
 from rest_framework.response import Response
-from rest_framework.views import exception_handler
+from rest_framework.views import set_rollback
 
 
 def custom_exception_handler(exc, context):
     # Call REST framework's default exception handler first,
     # to get the standard error response.
-    response = exception_handler(exc, context)
-    return Response(
-        {
-            "code": "40005",
-            "result": False,
-            "message": response.data,
-            "data": None,
-        }
-    )
+    if isinstance(exc, Http404):
+        exc = exceptions.NotFound()
+    elif isinstance(exc, PermissionDenied):
+        exc = exceptions.PermissionDenied()
+
+    if isinstance(exc, exceptions.APIException):
+        headers = {}
+        if getattr(exc, 'auth_header', None):
+            headers['WWW-Authenticate'] = exc.auth_header
+        if getattr(exc, 'wait', None):
+            headers['Retry-After'] = '%d' % exc.wait
+
+        if isinstance(exc.detail, (list, dict)):
+            data = exc.detail
+        else:
+            data = {'detail': exc.detail}
+        if exc.status_code == 400:
+            return Response(
+                {
+                    "code": "40005",
+                    "result": False,
+                    "message": data,
+                    "data": None,
+                }
+            )
+        set_rollback()
+        return Response(data, status=exc.status_code, headers=headers)
+
+    return None
--- awards_apply/utils/permission.py
+++ awards_apply/utils/permission.py
@@ -1,7 +1,6 @@
-from rest_framework import permissions
-
-from awards_apply.models import Admin
+from awards_apply.models import Admin, AwardApplicationRecord
 from awards_apply.utils.const import permission_denied
+from rest_framework import permissions
 
 
 class AssignSecretaryPermission(permissions.BasePermission):
@@ -16,3 +15,20 @@ class AssignSecretaryPermission(permissions.BasePermission):
             admin_username=request.user.username
         ).exists()
         return has_permission
+
+
+class ApprovalPermission(permissions.BasePermission):
+    """
+    审批权限校验
+    """
+
+    message = permission_denied()
+
+    def has_permission(self, request, view):
+        if request.method == "GET":
+            return True
+        application = AwardApplicationRecord.objects.filter(id=request.data["id"]).first()
+        if application:
+            if request.user.username in application.approval_users[application.approval_turn]:
+                return True
+        return False
