d2145c7e401e920e61e9dc0803ea0e1308ee51d9
BianCheng <1194832004@qq.com>
2022-02-17 09:27:35 +0800

    feature: 我的申请记录接口实现 #issue 63

--- awards_apply/views/__init__.py
+++ awards_apply/views/__init__.py
@@ -1,4 +1,4 @@
 from awards_apply.views.file_handle import download, upload  # noqa
 from awards_apply.views.secretary import SecretaryViewSet  # noqa
 from awards_apply.views.user import UserView  # noqa
-from awards_apply.views.award_view import AwardView, home, AvailableAwardsView, RecordView, ApplyedRecordView # noqa
+from awards_apply.views.award_view import AwardView, home, AvailableAwardsView, RecordView, ApplyedRecordView, MyRecordView # noqa
