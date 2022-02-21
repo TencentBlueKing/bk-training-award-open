c4e637ee81cbc9d2f66fb2bc4dac4538053cbd7b
yml <1138194104@qq.com>
2022-02-21 13:39:39 +0800

    feature: 添加数据库迁移文件,修改创建组存在提示信息(#40)

--- awards_apply/serializers/secretary_serializer.py
+++ awards_apply/serializers/secretary_serializer.py
@@ -1,9 +1,12 @@
 from awards_apply.models import Secretary
-from rest_framework import serializers
+from rest_framework import serializers, validators
 
 
 class SecretarySerializer(serializers.ModelSerializer):
     secretaries = serializers.JSONField(label="秘书信息列表", initial=list)
+    group_id = serializers.IntegerField(
+        validators=[validators.UniqueValidator(Secretary.objects.all(), message="组已创建")]
+    )
 
     class Meta:
         model = Secretary
