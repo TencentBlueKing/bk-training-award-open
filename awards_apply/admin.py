c67f330f0f8a6f843372f62f0cbefee954912c33
yml <1138194104@qq.com>
2022-02-21 22:45:32 +0800

    bugfix: 修复管理员表无初始化数据(#93)

--- awards_apply/admin.py
+++ awards_apply/admin.py
@@ -1,3 +1,11 @@
-
+from awards_apply.models import Admin
+from django.contrib import admin
 
 # Register your models here.
+
+
+class AdminDisplay(admin.ModelAdmin):
+    list_display = ["id", "admin_username"]
+
+
+admin.site.register(Admin, AdminDisplay)
