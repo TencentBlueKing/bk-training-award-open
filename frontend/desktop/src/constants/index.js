6c1ceea28c12c72c8128fe8ff262a2913b00fd75
couriourc <58319769+couriourc@users.noreply.github.com>
2022-02-22 19:05:05 +0800

    Feature 完善状态常量表 /#101 (#106)
    * bugfix: 完善空值校验 #99
    
    * bugfix: 解决标题始终为 index 的问题  #100
    
    * feature: 完善状态常量表   #101

--- frontend/desktop/src/constants/index.js
+++ frontend/desktop/src/constants/index.js
@@ -3,20 +3,49 @@
 /**
  * 奖项申请状态
  * */
-const NOT_APPLY = 0
-const APPLYED = 1
+export const NOT_APPLY = 0
+export const APPLYED = 1
+export const ENDED = 2
 export const APPROVAL_STATE = {
     [NOT_APPLY]: NOT_APPLY,
     [APPLYED]: APPLYED
 }
 export const AWARD_APPROVAL_STATE_MAP = {
-    [NOT_APPLY]: '未申请',
-    [APPLYED]: '申请中'
+    [NOT_APPLY]: '未开始',
+    [APPLYED]: '申请中',
+    [ENDED]: '已结束'
 }
 export const AWARD_APPROVAL_STATE_EN_MAP = {
     [NOT_APPLY]: 'not_apply',
-    [APPLYED]: 'applyied'
+    [APPLYED]: 'applyied',
+    [ENDED]: 'ended'
 }
+
+/***
+ * 申请表状态
+ *
+ * */
+export const REVIEW_PENDING = 0
+
+export const REVIEW_PASSED = 1
+
+export const REVIEW_NOT_PASSED = 2
+
+export const DRAFT = 3
+
+export const APPLY_APPROVAL_STATE_MAP = {
+    [REVIEW_PENDING]: '待评审',
+    [REVIEW_PASSED]: '评审通过',
+    [REVIEW_NOT_PASSED]: '评审不通过',
+    [DRAFT]: '草稿'
+}
+export const APPLY_APPROVAL_STATE_EN_MAP = {
+    [REVIEW_PENDING]: 'review_pending',
+    [REVIEW_PASSED]: 'review_passed',
+    [REVIEW_NOT_PASSED]: 'review_not_passed',
+    [DRAFT]: 'draft'
+}
+
 /**
  * 组类型
  * */
@@ -25,6 +54,10 @@ const GROUP = 1
 export const AWARD_LEVEL_MAP = {
     [COMPANY]: '公司',
     [GROUP]: '小组'
+}
+export const AWARD_LEVEL_EN_MAP = {
+    [COMPANY]: 'company',
+    [GROUP]: 'group'
 }
 /***
  * 组人员缓存键值
