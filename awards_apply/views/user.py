ddf6402f21533fbbe41fcfeabd656169b66c09ce
yml <1138194104@qq.com>
2022-02-22 22:20:03 +0800

    feature: 获取秘书可管理组接口, userinfo注入is_admin,is_secretary字段(#110)

--- awards_apply/views/user.py
+++ awards_apply/views/user.py
@@ -1,3 +1,4 @@
+from awards_apply.models import Admin, Secretary
 from blueking.component.shortcuts import get_client_by_request
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -13,6 +14,13 @@ class UserView(APIView):
         client = get_client_by_request(request)
         # 默认id参数使用username,这样获取用户信息就不用传参了
         query_params = {"id": request.user.username, **request.query_params}
+        # 为当前用户数据中添加is_admin, is_secretary字段, 用于前端页面展示判断
+        if api == "retrieve_user" and not request.query_params:
+            data = getattr(client.usermanage, api)(query_params)
+            is_admin = Admin.objects.filter(admin_username=request.user.username).exists()
+            is_secretary = Secretary.objects.filter(secretaries__contains=request.user.username).exists()
+            data["data"].update({"is_admin": is_admin, "is_secretary": is_secretary})
+            return Response(data)
         # 获得CompomentApi并调用
         data = getattr(client.usermanage, api)(query_params)
         return Response(data)
