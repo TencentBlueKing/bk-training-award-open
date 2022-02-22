137d376b17c2b6006830785e8ad29e26663a3905
couriourc <58319769+couriourc@users.noreply.github.com>
2022-02-22 19:05:31 +0800

    bugfix: 修复文件上传问题  #102  (#107)
    * bugfix: 完善空值校验 #99
    
    * bugfix: 解决标题始终为 index 的问题  #100
    
    * feature: 完善状态常量表   #101
    
    * bugfix: 修复文件上传问题 #102

--- frontend/desktop/src/api/service/apply-service.js
+++ frontend/desktop/src/api/service/apply-service.js
@@ -2,3 +2,40 @@
  * @file service apply
  * @author couriourc
  */
+import http from '@/api'
+
+export function getApproval (page, size) {
+    return http.get('/approval/', {
+        params: {
+            page,
+            size
+        }
+    })
+}
+
+/**
+ * @param { number } page 页码
+ * @param { number } size 每页大小
+ * */
+export function getRecord (page, size) {
+    return http.get('record/', {
+        params: {
+            page,
+            size
+        }
+    })
+}
+
+/**
+ * @param { number } id 申请id
+ * @param {string} action 通过为1,打回为0
+ * @param {string} approval_text 审批信息
+ * */
+
+export function postApproval ({ id, action, approval_text }) {
+    return http.post('/approval/', {
+        id,
+        action,
+        approval_text
+    })
+}
