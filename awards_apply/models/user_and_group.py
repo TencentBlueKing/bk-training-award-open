4952d6278d804567af2a2c364c969dc476ca7a22
404-error-404 <1791781644@qq.com>
2022-03-19 02:10:48 +0800

    feature: 完成组、消息相关接口

--- awards_apply/models/user_and_group.py
+++ awards_apply/models/user_and_group.py
@@ -24,6 +24,15 @@ class User(TimeBasic):
     def __str__(self):
         return self.username
 
+    def to_json(self):
+        return {
+            "user_id": self.id,
+            "username": self.username,
+            "display_name": self.display_name,
+            "phone": self.phone,
+            "email": self.email
+        }
+
 
 class Group(TimeBasic):
     full_name = models.CharField(max_length=128, unique=True, verbose_name="组名字")
@@ -62,26 +71,52 @@ class GroupApply(TimeBasic):
         (2, "未通过"),
     ]
     status = models.IntegerField(choices=APPLY_STATE, default=0, verbose_name="申请状态")
-    approver = models.CharField(max_length=128, blank=True, verbose_name="审批人账号")
-    approver_display_name = models.CharField(max_length=128, blank=True, verbose_name="审批人姓名")
+    approver = models.CharField(max_length=128, null=True, blank=True, verbose_name="审批人账号")
+    approver_display_name = models.CharField(max_length=128, null=True, blank=True, verbose_name="审批人姓名")
 
     def __str__(self):
-        return f"{self.username}({self.display_name})申请加入{self.group_name}: {self.APPLY_STATE[self.status]}"
+        return f"{self.username}({self.display_name})申请加入 {self.group_name}: {self.get_status_display()}"
+
+    def to_json(self):
+        return {
+            "apply_id": self.id,
+            "group_id": self.group_id,
+            "group_name": self.group_name,
+            "username": self.username,
+            "display_name": self.display_name,
+            "status": self.get_status_display(),
+            "create_time": self.create_time,
+            "update_time": self.update_time
+        }
 
 
 class Notification(TimeBasic):
     group_id = models.IntegerField(verbose_name="组id")
-    group_name = models.CharField(max_length=128, unique=True, verbose_name="组名字")
+    group_name = models.CharField(max_length=128, verbose_name="组名字")
     NOTIFY_TYPE = [
         (0, "group_user"),  # 组管理信息
-        (1, "award")        # 奖项管理信息
+        (1, "award")  # 奖项管理信息
     ]
     action_type = models.IntegerField(choices=NOTIFY_TYPE, verbose_name="消息类型")
     action_target = models.CharField(max_length=128, verbose_name="通知对象账号")
     action_username = models.CharField(max_length=128, verbose_name="该操作处理人账号")
     action_display_name = models.CharField(max_length=128, verbose_name="该操作处理人姓名")
-    message = models.CharField(max_length=128, verbose_name="消息主体")     # 比如已通过您的申请
+    message = models.CharField(max_length=128, verbose_name="消息主体")  # 比如已通过您的申请
     is_read = models.BooleanField(default=False, verbose_name="是否已读")
 
     def __str__(self):
         return f"{self.group_name}: {self.action_username}({self.action_display_name}){self.message}"
+
+    def to_json(self):
+        return {
+            "msg_id": self.id,
+            "group_id": self.group_id,
+            "group_name": self.group_name,
+            "action_type": self.get_action_type_display(),
+            "action_username": self.action_username,
+            "action_display_name": self.action_display_name,
+            "message": self.message,
+            "is_read": self.is_read,
+            "create_time": self.create_time,
+            "update_time": self.update_time
+        }
