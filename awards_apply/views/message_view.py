ff14f8ba2fd164689dd516ec5148bc093870d785
404-error-404 <1791781644@qq.com>
2022-03-20 16:25:52 +0800

    bugfix: 分页增加previous和next

--- awards_apply/views/message_view.py
+++ awards_apply/views/message_view.py
@@ -11,7 +11,7 @@ from rest_framework.views import APIView
 from awards_apply.models import Notification
 from awards_apply.serializers.notification_serializer import NotificationSerializers
 from awards_apply.utils.const import success_code, false_code, page_num_exception, value_exception
-from awards_apply.utils.pagination import PagePagination
+from awards_apply.utils.pagination import CommonPaginaation
 
 
 class MessageView(APIView):
@@ -19,13 +19,11 @@ class MessageView(APIView):
     def get(self, request):
         username = request.user.username
         msgs = Notification.objects.filter(action_target=username).order_by("-update_time")
-        pagination = PagePagination()
-        try:
-            pager_roles = pagination.paginate_queryset(queryset=msgs, request=request, view=self)
-            ser = NotificationSerializers(instance=pager_roles, many=True)
-            return JsonResponse(success_code(pagination.get_paginated_response(ser.data)))
-        except NotFound:
-            return JsonResponse(page_num_exception())
+        pagination = CommonPaginaation()
+        pager_roles = pagination.paginate_queryset(queryset=msgs, request=request, view=self)
+        ser = NotificationSerializers(instance=pager_roles, many=True)
+        response = pagination.get_paginated_response(ser.data)
+        return response
 
     # 修改通知状态为已阅
     def put(self, request):
