a4fc6905772e45dbadda7480f59991fe25a09275
couriourc <3234853521@qq.com>
2022-03-23 21:12:29 +0800

    bugfix: 退出小组按钮第一次点击无反应，且退出无刷新页面 #149

---
+++ frontend/desktop/src/components/self-dialog/index.vue
@@ -0,0 +1,48 @@
+<template>
+    <bk-dialog v-model="visible"
+        :header-position="'left'"
+        :loading="loading"
+        @confirm="confirm"
+        @cancel="cancel"
+        @value-change="$emit('change',$event)"
+        :mask-close="false"
+        :ok-text="$attrs['ok-text']"
+        :cancel-text="$attrs['cancel-text']"
+        :title="$attrs['title']"
+    >
+        <slot></slot>
+    </bk-dialog>
+</template>
+
+<script>
+    export default {
+        name: 'index',
+        model: {
+            prop: 'visible',
+            event: 'change'
+        },
+        props: {
+            visible: {
+                type: Boolean,
+                default: () => false
+            }
+        },
+        data () {
+            return {
+                loading: false
+            }
+        },
+        methods: {
+            confirm () {
+                this.$emit('confirm')
+            },
+            cancel () {
+            
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
